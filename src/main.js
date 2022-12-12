6d0ac9acf59ab172c7a1d3cd07ad6fd658da9d30
Iurii Pototskii <iuriipototskii2gmail.com>
2022-12-12 18:40:07 +0100

    feat: render main state at main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import UserProfileView from './view/user-profile-view.js';
 import FilterBarView from './view/filters-bar-view.js';
+import FilmWrapperView from './view/film-wrapper-view.js';
+import FooterStatisticView from './view/footer-statistic-view.js';
 import {render} from './util/render.js';
 
 const pageHeader = document.querySelector('.header');
 const pageMain = document.querySelector('.main');
+const pageFooterStatistics = document.querySelector('.footer__statistics');
 
 render(new UserProfileView(), pageHeader);
 render(new FilterBarView(), pageMain);
+render(new FilmWrapperView(), pageMain);
+render(new FooterStatisticView(), pageFooterStatistics);
