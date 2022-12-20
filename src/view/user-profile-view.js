bcdf2be2ffc46e16ec6da057d3391adfa190e770
Iurii Pototskii <iuriipototskii2gmail.com>
2022-12-20 12:11:25 +0100

    refactor: else remove and fix magic numbers

--- src/view/user-profile-view.js
+++ src/view/user-profile-view.js
@@ -1,27 +1,30 @@
 import {createElement} from '../util/render.js';
 
 function createUserProfileTemplate(alreadyWatched) {
+  const WATCHED_FILMS_TO_BE_FAN = 11;
+  const WATCHED_FILMS_TO_BE_MOVIE_BUFF = 21;
+  let profileRating = 'novice';
+
   if (alreadyWatched === 0) {
     return '';
-  } else {
-    let profileRating = 'novice';
-    if (alreadyWatched >= 11 && alreadyWatched <= 20) {
-      profileRating = 'fan';
-    } else if (alreadyWatched >= 21) {
-      profileRating = 'movie buff';
-    }
-
-    return (
-      `<section class="header__profile profile">
-        <p class="profile__rating">${profileRating}</p>
-        <img class="profile__avatar"
-            src="images/bitmap@2x.png"
-            alt="Avatar"
-            width="35"
-            height="35">
-      </section>`
-    );
   }
+
+  if (alreadyWatched >= WATCHED_FILMS_TO_BE_FAN && alreadyWatched < WATCHED_FILMS_TO_BE_MOVIE_BUFF) {
+    profileRating = 'fan';
+  } else if (alreadyWatched >= WATCHED_FILMS_TO_BE_MOVIE_BUFF) {
+    profileRating = 'movie buff';
+  }
+
+  return (
+    `<section class="header__profile profile">
+      <p class="profile__rating">${profileRating}</p>
+      <img class="profile__avatar"
+          src="images/bitmap@2x.png"
+          alt="Avatar"
+          width="35"
+          height="35">
+    </section>`
+  );
 }
 
 export default class UserProfileView {
